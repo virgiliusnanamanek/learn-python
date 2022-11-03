f361218b013e70e782778a3aafe4b68dacb3c117
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-03 17:06:10 +0700

    loop: while

---
+++ loops/while-loops.py
@@ -0,0 +1,15 @@
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+# The break Statement
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
