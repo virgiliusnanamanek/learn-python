39b1f5bd363297b469bbd1955ce46aa1f926fe4d
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-03 17:21:48 +0700

    function: main

--- function/operators.py
+++ function/operators.py
@@ -3,3 +3,17 @@ def adding(a, b):
 
 
 adding(5, 7)
+
+
+def multiplying(a, b):
+    print(a * b)
+
+
+multiplying(4, 89)
+
+
+def my_name(fname, lname):
+    print(fname + " " + lname)
+
+
+my_name("Emil", "Refsnes")
